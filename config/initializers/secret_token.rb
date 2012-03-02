fbd122456188fb6112d2af13421edb445e3d120a
Daniel R. Dennler <drdennler@elitemail.org>
2012-03-02 13:59:22 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'ce6728ea27fda0a945af39fcf2b6650061bc0b62e54a511d46ea789aafe40c0e077eb14ae7ea99fe6833ed1f57f285185132a8955a7b18a1d398d7410a407d40'
